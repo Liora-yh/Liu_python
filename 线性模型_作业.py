839d4686f2f29ee9d816a4657d5758feb6ca1d41
落雨花 <yuhualuo043@gmail.com>
2026-08-21 22:04:05 +0800

    初始提交：刘老师课程代码

---
+++ 线性模型_作业.py
@@ -0,0 +1,9 @@
+"""
+    Try to use the model, and draw the cost graph
+    Linear Model: y(尖) = x * w + b
+    # 作业：画出线性模型y(尖) = x * w + b的损失3D曲面图
+
+    任务：给定线性回归，使用MSE均方误差损失，用np.meshgrid生成w、b网格，
+    向量化计算损失，绘制 3D cost 曲面。完整可直接运行 Python 代码：
+
+"""
